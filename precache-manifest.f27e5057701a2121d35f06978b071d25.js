d13ce34c7a2787c3cfd7377b0f948a1c042bd2db
reeganrajasekar <areeganrajasekar@gmail.com>
2023-04-17 04:25:39 +0530

    Final

---
+++ precache-manifest.f27e5057701a2121d35f06978b071d25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb02c9a9629d3d73ad6807127b586754",
+    "url": "/index.html"
+  },
+  {
+    "revision": "804aa48bf28f958c0ba8",
+    "url": "/static/css/main.d3e2d208.chunk.css"
+  },
+  {
+    "revision": "13dce321e771dced57a2",
+    "url": "/static/js/2.5a05137b.chunk.js"
+  },
+  {
+    "revision": "804aa48bf28f958c0ba8",
+    "url": "/static/js/main.3b0daffe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
